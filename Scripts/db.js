23ea0ab9c25d859da01ccfca093f8867c3791b93
Ikopiko1999 <sakanovicismet99@gmail.com>
2026-03-23 23:27:20 +0100

    Initialer Projekt upload

---
+++ Scripts/db.js
@@ -0,0 +1,91 @@
+// Hier kommt der array rein
+let myDishes = [
+    {
+        "name": "Veggie mushroom black burger",
+        "price": 16.90,
+        "description": "Mixed green salad, Tomatoes, Edamame, Mushrooms",
+        "id": 1,
+    },
+    {
+        "name": "All meat burger",
+        "price": 15.90,
+        "description": "Beef, Bacon, Dill pickles, Smoked cheese, Ketchup, BBQ souse",
+        "id": 2,
+    },
+    {
+        "name": "Beef red burger",
+        "price": 14.90,
+        "description": "Beef, Cheese, Tomatoes, Lettuce, Onion",
+        "id": 3,
+    },
+    {
+        "name": "BIg chicken burger",
+        "price": 15.90,
+        "description": "Chicken, Cheese, Tomatoes, Lettuce, Onion, Bell pepper",
+        "id": 4,
+    },
+    {
+        "name": "Pizza Margherita",
+        "price": 11.90,
+        "description": "Tomato Sauce, Mozzarella",
+        "id": 5,
+    },
+    {
+        "name": "Pizza Chorizo",
+        "price": 13.90,
+        "description": "Tomato slices, Mozzarella, Chorizo",
+        "id": 6,
+    },
+    {
+        "name": "Funghi",
+        "price": 12.90,
+        "description": "Red onion, Olives, Button Mushrooms, Mozzarella",
+        "id": 7,
+    },
+    {
+        "name": "Quattro Formaggi with Chicken ",
+        "price": 15.90,
+        "description": "Chicken, Mozzarella, Gorgonzola, Fontina,  Parmigiano Reggiano",
+        "id": 8,
+    },
+    {
+        "name": "Warm beef arugula salad",
+        "price": 16.90,
+        "description": "Beef, Arugula, Field salad, Greek feta, Cherry tomatoes, Sun-dried Tomatoes, Balsamic-vinegar dressing",
+        "id": 9,
+    },
+    {
+        "name": "Mini green Salad",
+        "price": 7.90,
+        "description": "Green salad, Cucumber, Carrots, Parsley, Radishes ",
+        "id": 10,
+    },
+    {
+        "name": "Green Salad with sea food",
+        "price": 16.90,
+        "description": "Mixed greens, Cherry tomatoes, Red onion, Mussels, Squid rings, Shrimp, Dijon mustard-lemon dressing with dill",
+        "id": 11,
+    },
+    {
+        "name": "Vegan green salad with tofu",
+        "price": 14.90,
+        "description": "Green salad, Cherry tomatoes, Cucumber, Baby spinach, Edamame, Radishes, Bittercress, Tofu, Peanuts",
+        "id": 12,
+    },
+]
+
+
+let myImages = [
+    "./Assets/img/Burger_1.png",
+    "./Assets/img/Burger_2.png",
+    "./Assets/img/Burger_3.png",
+    "./Assets/img/Burger_4.png",
+    "./Assets/img/Pizza_5.png",
+    "./Assets/img/Pizza_6.png",
+    "./Assets/img/Pizza_7.png",
+    "./Assets/img/Pizza_8.png",
+    "./Assets/img/Salad_9.png",
+    "./Assets/img/Salad_10.png",
+    "./Assets/img/Salad_11.png",
+    "./Assets/img/Salad_12.png",
+]
